6bb7a38636f346da9d0803cf919b60c276e710a4
Jorge J Barroso <jorge@karumi.com>
2016-12-26 23:40:08 +0100

    Add search test.

--- app/src/androidTest/java/com/example/audiolibros/MainActivityTest.java
+++ app/src/androidTest/java/com/example/audiolibros/MainActivityTest.java
@@ -16,9 +16,13 @@ import io.victoralbertos.device_animation_test_rule.DeviceAnimationTestRule;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static android.support.test.espresso.action.ViewActions.replaceText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withParent;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static com.example.audiolibros.testutils.ChildAtPositionMatcher.childAtPosition;
 import static org.hamcrest.Matchers.allOf;
@@ -68,6 +72,28 @@ public class MainActivityTest {
         onView(childAtPosition(withId(R.id.recycler_view), 0)).perform(click());
 
         onView(withId(R.id.titulo)).check(matches(withText("Kappa")));
+    }
+
+    @Test
+    public void shouldShowSearchedBookWhenSearchABook() {
+        startActivity();
+
+        writeOnSearch("Kappa");
+
+        assertBookName("Kappa");
+
+    }
+
+    private void writeOnSearch(String textToSearch) {
+        ViewInteraction actionMenuItemView = onView(
+                allOf(withId(R.id.menu_buscar), withContentDescription("Buscar"), isDisplayed()));
+        actionMenuItemView.perform(click());
+        ViewInteraction searchAutoComplete = onView(
+                allOf(withId(R.id.search_src_text),
+                        withParent(allOf(withId(R.id.search_plate),
+                                withParent(withId(R.id.search_edit_frame)))),
+                        isDisplayed()));
+        searchAutoComplete.perform(replaceText(textToSearch), closeSoftKeyboard());
     }
 
     private void startActivity() {
